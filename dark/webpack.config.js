38834347a23159a47f5969296b81f7f00138dfa4
Joel Perez <joel.perez@rocketcom.com>
2021-04-01 13:47:52 -0600

    initial setup complete

--- dark/webpack.config.js
+++ dark/webpack.config.js
@@ -1,10 +1,12 @@
+const autoprefixer = require("autoprefixer");
+
 module.exports = [
   {
-    entry: "./app.scss",
+    entry: ["./app.scss", "./app.js"],
     output: {
       // This is necessary for webpack to compile
       // But we never use style-bundle.js
-      filename: "style-bundle.js",
+      filename: "bundle.js",
     },
     module: {
       rules: [
@@ -20,6 +22,14 @@ module.exports = [
             { loader: "extract-loader" },
             { loader: "css-loader" },
             {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [autoprefixer()],
+                },
+              },
+            },
+            {
               loader: "sass-loader",
               options: {
                 // Prefer Dart Sass
@@ -27,9 +37,19 @@ module.exports = [
 
                 // See https://github.com/webpack-contrib/sass-loader/issues/804
                 webpackImporter: false,
+                sassOptions: {
+                  includePaths: ["./node_modules"],
+                },
               },
             },
           ],
+        },
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
       ],
     },
