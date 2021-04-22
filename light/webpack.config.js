e20aaf88b8a225eb4b9fb9d1ef725bcbbd0deeda
Joel Perez <joel.perez@rocketcom.com>
2021-04-22 10:16:36 -0600

    copied dark theme files

---
+++ light/webpack.config.js
@@ -0,0 +1,63 @@
+const autoprefixer = require("autoprefixer");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = [
+  {
+    entry: ["./app.scss", "./app.js"],
+    output: {
+      // This is necessary for webpack to compile
+      // But we never use style-bundle.js
+      path: path.resolve(__dirname, "dist"),
+      filename: "bundle.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        inject: false,
+      }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "bundle.css",
+              },
+            },
+            { loader: "extract-loader" },
+            { loader: "css-loader" },
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: [autoprefixer()],
+                },
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                // Prefer Dart Sass
+                implementation: require("sass"),
+                sassOptions: {
+                  includePaths: ["./node_modules"],
+                },
+              },
+            },
+          ],
+        },
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      ],
+    },
+  },
+];
